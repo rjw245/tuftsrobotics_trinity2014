07c839115b9e33cd63acab58ef841db0b8079946
Riley Wood <rjw245@gmail.com>
2014-03-07 15:24:42 -0500

    Started coding

---
+++ tuftsrobotics_trinity2014_sketch/tuftsrobotics_trinity2014_sketch.ino
@@ -0,0 +1,18 @@
+//DEFINE ALL PINS HERE
+#define lineLeftPin   A14
+#define lineRightPin  A13
+#define distRight1Pin A12
+#define distRight2Pin A11
+#define distFrontPin  A10
+#define distLeft2Pin  A9
+#define distLeft1Pin  A8
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
